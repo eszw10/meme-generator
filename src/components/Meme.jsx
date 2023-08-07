30a9233048ccb9800e67d94f3ff1704cd9cd07b2
eryc_s.z.w <erycszw10@gmail.com>
2023-08-07 19:45:56 +0700

    fix state management

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+import memesData from "../data/MemesData";
+
 const Meme = () => {
-    // function randomURL() {
-    //     console.log(memesData["data"]["memes"][Math.floor(Math.random()*10)]["url"])
-    // }
+    const [meme,setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    function getMemeImage() {
+        let memesArray = memesData.data.memes
+        setMeme(meme => {
+            return {
+                ...meme,
+                randomImage : memesArray[Math.floor(Math.random()*memesArray.length)].url
+            }
+        })
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(meme => {
+            return {
+                ...meme,
+                [name] : value
+            }
+        })
+    }
+
     return (
         <form action="" className="main-container">
-            <input type="text" placeholder="top text" className="input-meme"/>
-            <input type="text" placeholder="bottom text" className="input-meme"/>
-            <button type="submit" className="meme-btn">Get a new meme image 🖼</button>
+            <input type="text" placeholder="top text" className="input-meme" name="topText" onChange={handleChange} value={meme.topText}/>
+            <input type="text" placeholder="bottom text" className="input-meme" name="bottomText" onChange={handleChange} value={meme.bottomText}/>
+            <button type="button" className="meme-btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
+            <div className="meme">
+                <img className="meme-image" src={meme.randomImage} alt="" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </form>
     );
 }
